e325d1fb246723a8d7cdad71481ef5f42b522f89
Jeferson <jefersonmoitinho20@gmail.com>
2019-05-17 21:43:46 -0300

    Initial commit

---
+++ ProjetoEscola/src/main/java/br/edu/ifsc/ui/Strings.java
@@ -0,0 +1,11 @@
+package br.edu.ifsc.ui;
+
+public class Strings {
+	public static final String btnLogin = "Entrar";
+	public static final String lblLogin = "Digite seu nome e selecione sua senha: ";
+	public static final String appTitle = "Hello UI";
+	public static final String lblMain = "Olá";
+	public static final String btnOk = "Ok";
+	public static final String loginError = "ERRO: DIGITE SEU NOME!";
+	public static final String passwordError = "Senha errada";
+}
